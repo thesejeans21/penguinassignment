d06cc85d1eac9990a6f101d28730dec00990c392
thesejeans21 <117754127+thesejeans21@users.noreply.github.com>
2022-12-08 17:37:41 +0000

    cleaning function used

---
+++ functions/cleaning.R
@@ -0,0 +1,9 @@
+#Cleaning function used to make penguins_clean from penguins_raw
+
+cleaning <- function(data_raw){
+  data_raw %>%
+    clean_names() %>%
+    remove_empty(c("rows", "cols")) %>%
+    select(-starts_with("delta")) %>%
+    select(-comments)
+}
